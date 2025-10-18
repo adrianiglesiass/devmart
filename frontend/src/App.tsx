abb09c3257ef0095d79f38aeb6b5980aaa8164d4
Adrián Iglesias Álvarez <adrianiglesias010@gmail.com>
2025-10-18 14:05:36 +0200

    feat: add auth context and react query (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { useAuth } from './context/AuthContext';
+
 function App() {
-	return (
-		<>
-			<h1>DevMart</h1>
-		</>
-	);
+  const { user, isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <h1>DevMart</h1>
+      {isAuthenticated ? <p>Bienvenido, {user?.email}!</p> : <p>No autenticado</p>}
+    </>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,19 @@ import { StrictMode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
