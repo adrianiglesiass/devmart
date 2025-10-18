cd6a3bebe0707e4b0d3d220e299d3f29d5ec68b1
Adrián Iglesias Álvarez <adrianiglesias010@gmail.com>
2025-10-18 23:08:28 +0200

    feat: add react router setup (#13)
    Add basic pages and protected routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext';
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <App />
+          <ReactQueryDevtools initialIsOpen={false} />
         </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
---
+++ frontend/src/pages/auth/Login.tsx
@@ -0,0 +1,56 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useState } from 'react';
+
+import { useAuth } from '@/context/AuthContext';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { login, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await login({ email, password });
+      navigate('/');
+    } catch (err: any) {
+      setError(err.message || 'Error al inciar sesión');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button
+          type="submit"
+          disabled={isLoading}
+        >
+          {isLoading ? 'Cargando...' : 'Login'}{' '}
+        </button>
+      </form>
+    </div>
+  );
+}
