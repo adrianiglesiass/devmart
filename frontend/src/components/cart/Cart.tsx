fd87d10edfb4582332dd8654f652df043b6ac681
adrianiglesiass <adrianiglesias010@gmail.com>
2025-10-31 18:15:30 +0100

    feat: add navigation to product detail from cart items

--- frontend/src/components/cart/Cart.tsx
+++ frontend/src/components/cart/Cart.tsx
@@ -63,6 +63,7 @@ export function Cart() {
                 <CartItem
                   key={item.id}
                   item={item}
+                  onClose={() => setOpen(false)}
                 />
               ))}
             </div>
